0a327a2448a14575bfab14d724d00fa547c23543
Melissa <>
2023-07-05 11:38:48 -0700

    Tested successfully using Melissa data files for 2023.06

--- MelissaAddressObjectWindowsPython3.ps1
+++ MelissaAddressObjectWindowsPython3.ps1
@@ -27,7 +27,7 @@ class DLLConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2023.05'
+$RELEASE_VERSION = '2023.06'
 $ProductName = "DQ_ADDR_DATA"
 
 # Uses the location of the .ps1 file 
