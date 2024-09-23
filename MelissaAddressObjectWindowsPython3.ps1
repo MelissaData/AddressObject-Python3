325b03830d48c5b71b3fe9ddc1996c8a9f9b6d3e
Melissa <>
2024-09-23 10:11:03 -0700

    Tested successfully using Melissa data files for 2024.09

--- MelissaAddressObjectWindowsPython3.ps1
+++ MelissaAddressObjectWindowsPython3.ps1
@@ -28,7 +28,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2024.08'
+$RELEASE_VERSION = '2024.09'
 $ProductName = "DQ_ADDR_DATA"
 
 # Uses the location of the .ps1 file 
